7565f9401f3adfaba9e270e3335c382b5fe7cd6b
ronkips <kipshillash@gmail.com>
2023-11-27 10:03:08 +0300

    implemented the registration part

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,5 +1,6 @@
 import Input from "@/components/input";
 import Image from "next/image";
+import axios from "axios";
 import React, { useCallback, useState } from "react";
 
 const Auth = () => {
@@ -13,6 +14,18 @@ const Auth = () => {
       currentVariant === "login" ? "register" : "login"
     );
   }, []);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        name,
+        email,
+        password
+      });
+    } catch (error) {
+      console.log("kuna mambo hapa", error);
+    }
+  }, [name, email, password]);
   return (
     <div className="relative h-[100vh] w-full bg-[url('/imgs/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -49,7 +62,10 @@ const Auth = () => {
                 onChange={(e: any) => setPassword(e.target.value)}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {variant === "login" ? "Login" : " Sign Up"}
             </button>
             <p className="text-neutral-500 mt-12">
