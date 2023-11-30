ea224963743880109079c41bd2fff4813af02843
ronkips <kipshillash@gmail.com>
2023-11-30 18:20:20 +0300

    updated the mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import NavItems from "./NavItems";
+import { BsChevronDown } from "react-icons/bs";
+import { BsChevronCompactUp } from "react-icons/bs";
+import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
+  const [showMenuMobile, setShowMenuMobile] = useState(false);
+  const toggleMobileMenu = useCallback(() => {
+    setShowMenuMobile((current) => !current);
+  }, []);
+
   return (
     <nav className=" w-full fixed z-40">
       <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90 ">
@@ -21,8 +29,17 @@ const Navbar = () => {
           <NavItems label="My List" />
           <NavItems label="Browse by Languages" />
         </div>
-        <div className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+        <div
+          onClick={toggleMobileMenu}
+          className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative"
+        >
           <p className="text-white text-sm">Browse</p>
+          {showMenuMobile ? (
+            <BsChevronCompactUp className="text-white transition" />
+          ) : (
+            <BsChevronDown className="text-white transition" />
+          )}
+          <MobileMenu visible={showMenuMobile} />
         </div>
       </div>
     </nav>
