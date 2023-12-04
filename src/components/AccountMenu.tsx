02b0c1f9e116a90da5518a017ce32eb9c3df763a
ronkips <kipshillash@gmail.com>
2023-12-04 22:05:28 +0300

    update

---
+++ src/components/AccountMenu.tsx
@@ -0,0 +1,34 @@
+import { signOut } from "next-auth/react";
+import Image from "next/image";
+import React from "react";
+interface AccountMenuProps {
+  visible?: boolean;
+}
+
+const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+  if (!visible) {
+    return null;
+  }
+  return (
+    <div className="bg-black w-40 absolute top-14 right-0 py-5 flex-col border-2  border-gray-800 flex ">
+      <div className=" flex flex-col gap-3">
+        <div className="px-3 group/item flex flex-grow items-center w-full">
+          <Image src="/imgs/user.png" alt="Avatar" width={30} height={30} />
+          <p className="text-white text-sm group/items-hover:underline">
+            Username
+          </p>
+        </div>
+        <div className="bg-gray-600 border-0 h-px my-4">
+          <div
+            onClick={() => signOut()}
+            className="px-3 py-2 text-center text-white"
+          >
+            Sidn Out
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountMenu;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,19 @@ import NavItems from "./NavItems";
 import { BsChevronDown } from "react-icons/bs";
 import { BsChevronCompactUp } from "react-icons/bs";
 import MobileMenu from "./MobileMenu";
+import { BsSearch, BsBell } from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
 
 const Navbar = () => {
   const [showMenuMobile, setShowMenuMobile] = useState(false);
+  const [showAccountMenu, setShoAccountMenu] = useState(false);
   const toggleMobileMenu = useCallback(() => {
     setShowMenuMobile((current) => !current);
   }, []);
 
+  const toggleAccountMenu = useCallback(() => {
+    setShoAccountMenu((current) => !current);
+  }, []);
   return (
     <nav className=" w-full fixed z-40">
       <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90 ">
@@ -40,6 +46,28 @@ const Navbar = () => {
             <BsChevronDown className="text-white transition" />
           )}
           <MobileMenu visible={showMenuMobile} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center ">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsBell />
+          </div>
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-5 lg:h-5 rounded-md overflow-hidden">
+              <Image src="/imgs/user.png" alt="Avatar" width={30} height={30} />
+            </div>
+            {showAccountMenu ? (
+              <BsChevronCompactUp className="text-white transition" />
+            ) : (
+              <BsChevronDown className="text-white transition" />
+            )}
+            <AccountMenu visible={showAccountMenu} />
+          </div>
         </div>
       </div>
     </nav>
