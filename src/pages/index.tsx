f8d06dacfbc80c5d08fcd27349d57a49fda2516b
ronkips <kipshillash@gmail.com>
2023-12-05 19:23:05 +0300

    created the billboard and fetcher part

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Billboard from "@/components/Billboard";
 import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
@@ -23,15 +24,8 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-6xl font-bold">Welcome to Next.js!</h1>
-        <button
-          className="mt-4 px-4 py-2 bg-red-500 text-white rounded-md"
-          onClick={() => signOut()}
-        >
-          Sign Out
-        </button>
-      </div>
+      <Billboard />
+    
     </>
   );
 }
