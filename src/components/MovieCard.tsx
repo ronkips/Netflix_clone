9c501defd62d4d4ac754a8bb5b894e5b4db43846
ronkips <kipshillash@gmail.com>
2024-01-09 10:53:00 +0300

    update

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -11,8 +11,20 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         src={data.thumbnailUrl}
         alt="thumbnail"
         draggable={false}
-        className="rounded-md object-cover cursor-pointer transition duration-200 ease-in-out transform group-hover:opacity-90 sm:group-hover:opacity-0 shadow-xl w-full h-[12vw]"
+        className="rounded-md object-cover cursor-pointer transition duration delay-300  group-hover:opacity-90 sm:group-hover:opacity-0 shadow-xl w-full h-[12vw]"
       />
+      <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
+        <img
+          className="cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]"
+          src={data.thumbnailUrl}
+          alt="tumbnail"
+        />
+        <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
+          <div className="flex flex-row items-center gap-3">
+            <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300 "></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
